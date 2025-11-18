7ff2d8261a6cce044f140951025bddc80f0f223c
Nikubwayo <esthernikubwayo2024@gmail.com>
2025-11-18 21:07:32 +0200

    Lab 1: Project setup and entity design

---
+++ src/main/java/org/example/employeesystem/Lab1/Entity/Employee.java
@@ -0,0 +1,23 @@
+package org.example.employeesystem.Lab1.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String position;
+    private String department;
+    private LocalDate hireDate;
+}
